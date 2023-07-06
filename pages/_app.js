1351d2e103220db9fbe285ef89299dcafee20ec7
Oriana E. Martínez <martinez.oriana.e@gmail.com>
2023-07-06 16:10:42 -0400

    [NEW] Initial App

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
+/* Styles */
+import '@/styles/globals.css';
 
+// FUNCION PRINCIPAL
+/* Aplicación */
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    // RETORNO
+    return (<>
+        <Head>
+            <title>Initial App</title>
+
+            <meta content='Initial App in Next.js' name='description' />
+        </Head>
+
+        <Component {...pageProps} />
+    </>);
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
+// FUNCION PRINCIPAL
+/* Marcado HTML */
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    // RETORNO
+    return (<Html lang="en">
+        <Head />
+        
+        <body>
+            <Main />
+
+            <NextScript />
+        </body>
+    </Html>);
 }
